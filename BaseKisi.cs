86bbebd698b0c267839937846532c741c3e961fa
pc <pc@DESKTOP-M7QJQNL>
2024-08-12 18:05:22 +0300

    Add project files.

---
+++ BaseKisi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratik_inheritance
+{
+    public class BaseKisi
+    {
+
+        public string Ad {  get; set; }
+        public string Soyad {  get; set; }
+        
+        public void Show()
+        {
+            Console.WriteLine($"Kişi adı ve soyadı = {Ad}{Soyad}");
+        }
+
+     
+
+    }
+
+    public class Ogrenci : BaseKisi
+    {
+        public int OgrNo { get; set; }
+
+        public void OgrnShow()
+        {
+            Console.WriteLine($"Ogr adı soyadı ve numarası = {Ad}{Soyad}{OgrNo}");
+        }
+
+
+
+    }
+
+    public class Ogretmen : BaseKisi
+    {
+        public int Maas { get; set; }
+
+
+        public void OgrtShow()
+        {
+            Console.WriteLine($"Ogrt adı soyadı ve maas = {Ad}{Soyad}{Maas}");
+        }
+
+
+    }
+
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using pratik_inheritance;
+
+Ogrenci kisi1 = new Ogrenci();
+
+kisi1.OgrNo = 1233423254;
+kisi1.Ad = "ahmet";
+kisi1.Soyad = "bilgin";
+
+kisi1.OgrnShow();
+
+
+
+Ogretmen kisi2 = new Ogretmen();
+
+kisi2.Maas = 25000;
+kisi2.Ad = "ahmet";
+kisi2.Soyad = "bilgin";
+
+kisi2.OgrtShow();
